6745ba3a69cee25681d397114f52920a60150060
Frank <frank.phan@helius-software.com>
2022-05-17 01:23:15 +0700

    Add project files.

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string server = "127.0.0.1";
+            string database = "test";
+            string username = "root";
+            string password = "Dathung091@123";
+            string connections = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+            MySqlConnection conn = new MySqlConnection(connections);
+            conn.Open();
+            string query = "Select * from Users";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<User> users = new List<User>();
+            while (reader.Read())
+            {
+                User user = new User
+                {
+                    Id = (int)reader["Id"],
+                    UserName = (string)reader["UserName"],
+                    Email = (string)reader["Email"],
+                    Password = (string)reader["Pass"]
+                };
+                users.Add(user);
+            }
+
+            return View(users);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
